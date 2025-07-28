161fda0156d45d884796b389d20ecd653dd47fb7
Florin-Robert Pap <165192808+RobertPap57@users.noreply.github.com>
2025-07-28 23:50:14 +0200

    feat(board): implement task display area with styled test card

--- js/pages/board/core.js
+++ js/pages/board/core.js
@@ -5,3 +5,31 @@ async function initBoard() {
     displayHeaderAvatar();
     highlightLink('board');
 }
+
+
+
+function getTaskCardHTML(task){
+    return `<article class="task-card">
+                            <header>
+                                <p class="task-category">User Story</p>
+                            </header>
+
+                            <h3>${task.title}</h3>
+
+                            <p class="task-desc">Task description</p>
+
+                            <div class="task-progress" role="progressbar" aria-valuenow="2" aria-valuemin="0"
+                                aria-valuemax="5">
+                                2/5 subtasks
+                            </div>
+
+                            <footer>
+                                <div class="assigned-avatars" aria-label="Assigned to: John, Lisa, Mike">
+                                    <!-- avatar icons -->
+                                </div>
+                                <div class="task-priority" aria-label="Priority: High">
+                                    <!-- icon -->
+                                </div>
+                            </footer>
+                        </article>`;
+} 
